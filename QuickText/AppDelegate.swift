be78cba32997a7dce7960c5f18b1a43e56b289f8
garroh <garroh99@gmail.com>
2017-08-12 15:29:29 -0700

    First working commit

---
+++ QuickText/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  QuickText
+//
+//  Created by RohGar on 8/5/17.
+//  Copyright © 2017 Rovag. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
